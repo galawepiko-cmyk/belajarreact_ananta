120273c139ddc39de65f7ef61565d537ab607513
galawepiko-cmyk <galawepiko@gmail.com>
2026-09-03 14:24:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
         <h1>Profile</h1>
         <Labelnama nama="Ananta" />
-        <Labelalamat alamat="jl. Apa Aja" />
+        <Labelalamat alamat="jl. Mangga besar XIX" />
 
         <EventExample />
   
