55382b847a17936e3b923d330dfbc57b7a4c4e6f
galawepiko-cmyk <galawepiko@gmail.com>
2026-09-03 14:24:20 +0700

    Update address in Labelalamat component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
         <h1>Profile</h1>
         <Labelnama nama="Ananta" />
-        <Labelalamat alamat="jl. Mangga besar XIX" />
+        <Labelalamat alamat="jl. Apa Aja" />
 
         <EventExample />
   
